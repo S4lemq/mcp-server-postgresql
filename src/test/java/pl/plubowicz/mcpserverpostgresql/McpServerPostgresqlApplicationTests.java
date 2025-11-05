6f0106f3bafd6a81c74ff51e58c61290c561f20c
Piotr <peter.lubowicz@gmail.com>
2025-11-05 22:29:02 +0100

    initialize mcp-server-postgresql project with Maven and Spring Boot setup

---
+++ src/test/java/pl/plubowicz/mcpserverpostgresql/McpServerPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.plubowicz.mcpserverpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
